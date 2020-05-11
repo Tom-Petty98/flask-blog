31b9c5ff65f0e767e02967613444b5de54ae6a9e
Tom Petty <tompetty527@yahoo.co.uk>
2020-05-11 15:37:22 +0000

    Connected database

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']= str(getenv('FLASK_BLOG_DB_URI'))
+db = SQLAlchemy(app)
 
 from application import routes
 
--- application/routes.py
+++ application/routes.py
@@ -1,23 +1,12 @@
 from flask import render_template
 from application import app
-
-blogData = [
-    {  
-        "name": {"first":"John", "last":"Doe"},
-        "title":"First Post",
-        "content":"This is some blog data for Flask lectures"
-    },
-    {   
-        "name": {"first":"Jane", "last":"Doe"},
-        "title":"Second Post",
-        "content":"This is even more blog data for Flask lectures"
-    }
-]
+from application.models import Posts
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', title="Home Page", posts=blogData)
+    postData = Posts.query.first()
+    return render_template('home.html', title="Home Page", post=postData)
 
 @app.route('/about')
 def about():
