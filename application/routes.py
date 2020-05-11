bc29335732af1722e6776b826c24af911d1f77b2
Tom Petty <tompetty527@yahoo.co.uk>
2020-05-11 12:06:04 +0000

    first commit

---
+++ application/routes.py
@@ -0,0 +1,25 @@
+from flask import render_template
+from application import app
+
+blogData = [
+    {  
+        "name": {"first":"John", "last":"Doe"},
+        "title":"First Post",
+        "content":"This is some blog data for Flask lectures"
+    },
+    {   
+        "name": {"first":"Jane", "last":"Doe"},
+        "title":"Second Post",
+        "content":"This is even more blog data for Flask lectures"
+    }
+]
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', title="Home Page", posts=blogData)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title="About Page")
+
