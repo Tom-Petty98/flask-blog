a81c45db0ac3382b4113e7d88f2fff5f5c733093
Tom Petty <tompetty527@yahoo.co.uk>
2020-05-12 10:45:18 +0000

    Adding a post create page

--- application/__init__.py
+++ application/__init__.py
@@ -5,6 +5,7 @@ from os import getenv
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI']= str(getenv('FLASK_BLOG_DB_URI'))
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 db = SQLAlchemy(app)
 
 from application import routes
