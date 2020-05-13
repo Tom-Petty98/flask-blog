b2c9a419e98ec403d73b297564f68730421e16c3
Tom Petty <tompetty527@yahoo.co.uk>
2020-05-13 10:51:42 +0000

    Login and authentication

--- application/__init__.py
+++ application/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 from os import getenv
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 app.config['SQLALCHEMY_DATABASE_URI']= str(getenv('FLASK_BLOG_DB_URI'))
 app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 db = SQLAlchemy(app)
+login_manager.login_view = 'login'
 
 from application import routes
 
