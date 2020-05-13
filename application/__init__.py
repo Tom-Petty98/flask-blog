a028cd8e44f593d2af53d765cbb325632f39585f
Tom Petty <tompetty527@yahoo.co.uk>
2020-05-13 09:54:49 +0000

    Registration page and db added

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 from os import getenv
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
 app.config['SQLALCHEMY_DATABASE_URI']= str(getenv('FLASK_BLOG_DB_URI'))
 app.config['SECRET_KEY'] = getenv('SECRET_KEY')
