fcb5af8bc88f17f90a5bccf55174f4cdb64ae037
Tom Petty <tompetty527@yahoo.co.uk>
2020-05-14 10:57:41 +0000

    Added update functionality

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, url_for, request
 from application import app, db, bcrypt
 from application.models import Posts, Users
-from application.forms import PostForm, RegistrationForm, LoginForm
+from application.forms import PostForm, RegistrationForm, LoginForm, UpdateAccountForm
 from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route('/')
@@ -74,6 +74,23 @@ def login():
             else:
                 return redirect(url_for('home'))
     return render_template('login.html', title='Login', form=form)
+
+
+@app.route('/account', methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.first_name = form.first_name.data
+        current_user.last_name = form.last_name.data
+        current_user.email = form.email.data
+        db.session.commit()
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.first_name.data = current_user.first_name
+        form.last_name.data = current_user.last_name
+        form.email.data = current_user.email
+    return render_template('account.html', title='Account', form=form)
 
 
 @app.route("/logout")
