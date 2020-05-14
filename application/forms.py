bb6854d366ad59efca14bca46a0a4439c2abb05e
Tom Petty <tompetty527@yahoo.co.uk>
2020-05-14 09:35:46 +0000

    updated Users and post database

--- application/forms.py
+++ application/forms.py
@@ -4,18 +4,6 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationE
 from application.models import Users
 
 class PostForm(FlaskForm):
-    first_name = StringField('First Name',
-        validators = [
-            DataRequired(),
-            Length(min=2, max=30)
-        ]
-    )
-    last_name = StringField('Last Name',
-        validators = [
-            DataRequired(),
-            Length(min=2, max=30)
-        ]
-    )
     title = StringField('Title',
         validators = [
             DataRequired(),
@@ -33,6 +21,18 @@ class PostForm(FlaskForm):
 
 
 class RegistrationForm(FlaskForm):
+    first_name = StringField('First Name',
+        validators = [
+            DataRequired(),
+            Length(min=2, max=30)
+        ]
+    )
+    last_name = StringField('Last Name',
+        validators = [
+            DataRequired(),
+            Length(min=2, max=30)
+        ]
+    )
     email = StringField('Email',
         validators = [
             DataRequired(),
